8f851137dc7042f2bb761a1ddc1d2bf85d675501
adriel <adrielarochaoronoz@gmail.com>
2022-09-19 19:44:10 +0100

    basic configuration ended

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-console.log('Hola node + express')
-// inicia aqui tu app de express
+import express from 'express'
+
+const app = express()
 
 
+app.get('/', (req, res)=>{
+    res.send({msg: 'hola mundo express'})
+})
+
+app.listen(8088,()=>{
+    console.log('server listen on port 8088');
+    
+})
